9176c497040f941672085d0eeb280f91683bc9f8
arunsridher <arunsridher@gmail.com>
2022-09-07 11:18:27 +0530

    added original resource

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -18,6 +18,10 @@ function App() {
           Learn React
         </a>
       </header>
+      <p>
+        Resource:
+        https://www.geeksforgeeks.org/how-to-deploy-your-react-websites-on-github/
+      </p>
     </div>
   );
 }
